62988ae5416ceef0e55b9b4601df3fe51fdb5a7f
Ebru Güleç <glcebru@hotmail.com>
2015-02-11 22:54:26 +0200

    Create enum.cpp

---
+++ enum.cpp
@@ -0,0 +1,38 @@
+# enum: Değişkenin alabileceği değerlerin belli(sabit) olduğu
+durumlarda programı daha okunabilir hale getirmek için kullanılır.
+#include <stdio.h>
+#include <stdlib.h>
+
+int tek (int);
+
+int main()
+{
+  enum boolean
+  {
+      false = 0,
+      true = 1
+  };
+
+  enum boolean sonuc;
+  int x;
+
+  printf("Bir sayi giriniz\n");
+  scanf("%d", &x);
+
+  sonuc = tek(x);
+
+  if(sonuc == true)
+
+        printf("\nGirilen sayi tektir.");
+
+    else if
+
+        printf("\nGirilen sayi cifttir");
+        
+    return 0;
+}
+
+int tek (int n)
+{
+    return ( n % 2);
+}
