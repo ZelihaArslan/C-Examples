56691fca271b0fa3d4aa228742318bcfdc825fc1
Ebru Güleç <glcebru@hotmail.com>
2015-02-20 20:33:13 +0200

    Create Ulama.cpp

---
+++ Ulama.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0, k = 0;
+    char cumle[200];
+    int kontrol = 0;
+    printf("Ulama icin bir cumle giriniz\n");
+    char sessiz[42]="BCDFGĞHJKLMNPRSŞTVYZbcdfgğhjklmnprsştvyz";
+    gets(cumle);
+
+    for(i = 0; cumle[i] != '\0'; i++ )
+    {
+         if(cumle[i] == ' ' && cumle[i+1] != ' ')
+            {
+            for(k =0; k < 43; k++)
+                {
+                    if(cumle[i-1] == sessiz[k] && cumle[i+1] != sessiz[k])
+                        kontrol++;
+                }
+            }
+    }
+    if(kontrol > 0)
+        printf("Ulama vardir.");
+    else
+        printf("Ulama yoktur.");
+
+    return 0;
+}
+
