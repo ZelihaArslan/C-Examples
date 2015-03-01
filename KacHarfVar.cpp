0e57afe61aac241769b286381248ac4c8b5a428d
Ebru Güleç <glcebru@hotmail.com>
2015-03-01 18:03:02 +0200

    Create KacHarfVar.cpp

---
+++ KacHarfVar.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char cumle[100];
+    int harfSay[100]={0};
+    int i,k,m,s=1,a=1;
+
+    printf("Bir cumle giriniz\n"),
+    gets(cumle);
+    while(cumle[s] != '\0')
+    {
+        s++;
+        a++;
+    }
+
+    for(i=0; i < s; i++)
+    {
+       if(harfSay[i] == 0)
+       {
+            for(k = i+1; k < s; k++)
+            {
+                if(cumle[k] != ' ' && cumle[i] == cumle[k])
+                {
+                    harfSay[i]++;
+                    harfSay[k]++;
+                }
+            }
+            if(cumle[i] != ' ')
+                printf("%c hafinden %d tane var\n", cumle[i], harfSay[i]+1);
+       }
+    }
+
+    return 0;
+}
+
